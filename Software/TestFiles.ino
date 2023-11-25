b728d624cf774a4815f986136dc4ea9226aba4bd
GeneralZouna <39312243+GeneralZouna@users.noreply.github.com>
2023-11-25 21:15:52 +0100

    Create TestFiles.ino
    I'm making a test program for the OrglceTM

---
+++ Software/TestFiles.ino
@@ -0,0 +1,133 @@
+#define DEBUG
+
+/*
+Pins for connection
+*/
+#define DATA 1
+#define CLK 2
+#define LATCH 3
+#define R_PIN 4
+
+#define FLIPPED_OUTPUTS //pins are NC
+
+/*
+Set true if every board is flipped by pins:
+Q7 is first LSB; Q1 is MSB
+*/
+#define FLIPPED_PORTS true  
+
+/*
+Number of pins that may be on at one time
+WARNING:Bigger number solows down code
+*/
+#define MAX_ON_PINS 50
+
+//On time for each output in ms
+int ON_TIME = 2000;  
+
+
+/*
+Get data from controller
+Read SD
+Update outputs
+*/
+
+int TimerArr_time[MAX_ON_PINS]; //array for storing ticks before switching off
+int TimerArr_pin[MAX_ON_PINS];  //array for storing what pin is connected to a timer
+
+int PortLength = 8;
+unsigned long old_time;
+
+ /*
+  *   Input commands:
+  *
+  *   ports: <int>      Sets number of outputs
+  *   mode: <byte/flag> chooses mode Program/Manual/Random
+  *   stop              turns all outputs to 0, stops execution of programs
+  *   start             starts the program
+  *   on_time           sets ON_TIME
+  *  
+  *
+  */
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  //InputRead();
+  updateOutputs();
+  old_time = millis();
+  delay(100);
+}
+
+
+
+
+
+
+bool TestOutput(int pin) {
+  int deltaTime = (int)(millis() - old_time);
+  // pin 0 doesn't exist --> code for empty
+  if (pin == 0) {
+    return false;
+  }
+
+  //Find pin and timer; if it's time is pass update it
+  for (int i = 0; i < MAX_ON_PINS; i++) {
+
+    /*
+    *  If you find pin updte it
+    *  If time has elapsed turn it off
+    */
+
+    if (TimerArr_pin[i] == pin) {
+      
+      TimerArr_time[i] -= deltaTime;
+      if (TimerArr_time[i] <= 0) {
+        TimerArr_pin[i] = 0;
+        TimerArr_time[i] = 0;
+        return false;
+      }
+      return true;
+    }
+  }
+
+  return false;
+}
+
+
+void updateOutputs() {
+  char outputbyte = 0;
+  char addBit = 0;
+
+  digitalWrite(R_PIN,HIGH);
+  digitalWrite(R_PIN,LOW);
+
+  for (int i = 1; i < PortLength+1; i++) {
+    addBit = TestOutput(i) ? 1 : 0;
+    outputbyte = outputbyte << 1 + addBit;
+
+#ifdef DEBUG
+    Serial.print( (int) addBit);
+#endif
+    
+    if (i-1 % 8 == 7) {
+      
+#ifdef FLIPPED_OUTPUTS
+    outputbyte = outputbyte ^ 255;
+#endif
+      
+      shiftOut(DATA, CLK, (FLIPPED_PORTS)? LSB:MSBFIRST, outputbyte);
+
+      outputbyte = 0;
+    }
+  }
+  digitalWrite(LATCH,LOW);
+  digitalWrite(LATCH,HIGH);
+  Serial.println("");
+}
