d631b3e8510d24c7935ec36dfa992f021b97ba8b
GeneralZouna <39312243+GeneralZouna@users.noreply.github.com>
2023-12-23 13:09:03 +0100

    Create SlowShift.ino
    Shiftout slowdown

---
+++ Software/FireworkSoftware/SlowShift.ino
@@ -0,0 +1,18 @@
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val)
+{
+	uint8_t i;
+
+	for (i = 0; i < 8; i++)  {
+		if (bitOrder == LSBFIRST) {
+			digitalWrite(dataPin, val & 1);
+			val >>= 1;
+		} else {	
+			digitalWrite(dataPin, (val & 128) != 0);
+			val <<= 1;
+		}
+		delay(SHIFT_DELAY);	
+		digitalWrite(clockPin, HIGH);
+    delay(SHIFT_DELAY);
+		digitalWrite(clockPin, LOW);		
+	}
+}
